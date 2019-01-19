815baf07afc57837c765426008a8c183841b3242
Edvin Boul <kittenparry@users.noreply.github.com>
2019-01-19 17:01:02 +0300

    Overall design (Colours)
    draw players/mid
    add underscores to colours to use them as classes

--- game/main.js
+++ game/main.js
@@ -2,4 +2,3 @@ const games = require('./games');
 const players = require('./players')
 
 var game = new games(4);
-document.getElementById('div_players').innerHTML += players.avatars[0].avatar;
--- game/main_window.js
+++ game/main_window.js
@@ -1,4 +1,5 @@
 const properties = require('./properties');
+//const players = require('./players');
 
 function draw_board(){
   top_row = '<tr>';
@@ -37,13 +38,29 @@ function draw_tiles(){
   tiles = properties.tiles;
   for(i=0;i<40;i++){
     tile = document.getElementById('tile_' + i);
-    tile.innerHTML = `${tiles[i].name}`;
+    tile.innerHTML = '';
     if(tiles[i].colour !== undefined){
-      tile.innerHTML += ` ${tiles[i].colour}`;
+      tile.innerHTML += `<div class='colour ${tiles[i].colour}'></div>`;
     }
+    tile.innerHTML += `${tiles[i].name}`;
+    tile.innerHTML += `<div class='tile_stop' id='tile_stop_${i}'></div>`;
   }
+}
+function draw_players(){
+  avatars = players.avatars;
+  doc = document.getElementById('div_players');
+  avatars.forEach(function(avatar){
+    temp = `<div class='player_profile'>${avatar.avatar} $${avatar.money}</div>`
+    doc.innerHTML += temp;
+  });
+}
+function draw_mid(){
+  game_mid = document.getElementById('game_mid');
+  game_mid.innerHTML += '<button>roll die</button>';
 }
 
 draw_board();
 draw_sidebar();
 draw_tiles();
+draw_players();
+draw_mid();
