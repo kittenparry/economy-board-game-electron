d8b022ddfd0cddb2ebc97265f3f13499fe65f084
Edvin Boul <kittenparry@users.noreply.github.com>
2019-10-13 15:24:02 +0300

    Match the Player class changes in 3rd iteration
    https://github.com/kittenparry/economy-board-game-3

--- game/players.js
+++ game/players.js
@@ -1,17 +1,19 @@
 func_basic = require('./players_func_basic');
 
 class Player extends func_basic {
-	constructor(avatar, ai) {
+	constructor(avatar, is_ai) {
 		super();
 		this.money = 1500;
-		this.properties = [] //remove this probably?
 		this.position = 0;
 		this.avatar = avatar;
 		this.double_die = 0;
-		this.ai = ai;
+		this.is_ai = is_ai;
 		this.bid = 0;
+		// TODO: probably a better way than this for auction?
 		this.get_out_of_jail = 0;
 		this.in_jail = false;
+		this.in_jail_turn = 0;
+		// start from 0, increment each time the turn is player's, require payment on third one if not a double die
 		this.is_bankrupt = false;
 	}
 }
